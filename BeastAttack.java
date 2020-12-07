f7cf0c6fae4672ba240330575c7b0184d9d3cf1c
Giannis Tyrovolas <johntyro@hotmail.com>
2020-12-07 14:45:05 +0000

    ready for checking

--- BeastAttack.java
+++ BeastAttack.java
@@ -12,7 +12,6 @@ public class BeastAttack
     }
 
     static void printCT(byte[] ct){
-	
 		for(int i = 0; i < 8; i++)
 		{
 		    System.out.print(String.format("%02x ", ct[i]));
@@ -37,97 +36,101 @@ public class BeastAttack
 
     static byte[] merge(int pos, byte[] msg, byte x){
     	byte[] prefix = new byte[8];
-	int i = 0;
-	for(; i < 7 - pos; i++) prefix[i] = 0;
-	for(; i < 7; i++) prefix[i] = msg[pos + i - 7];
-	prefix[7] = x;
+		int i = 0;
+		for(; i < 7 - pos; i++) prefix[i] = 0;
+		for(; i < 7; i++) prefix[i] = msg[pos + i - 7];
+		prefix[7] = x;
 
-	return prefix;
+		return prefix;
+    }
+
+    static void guessIV(byte[] ciphertext, byte[] prefix){
+    	int cnt = 0;
+    	boolean found = false;
+
+		while(!found && cnt < 300){
+			cnt += 1;
+			
+			expIV = nextIV(ciphertext);
+			callEncrypt(prefix, prefix.length, ciphertext);
+
+			found = Arrays.equals(expIV, subArray(ciphertext, 0, 8));
+		}
+
+		if(found) return;
+		else return -1;
     }
 
     public static void main(String[] args) throws Exception
     {
 
-	Scanner sc = new Scanner(System.in);
-        
-	byte[] ciphertext = new byte[1024]; 
-	byte[] prevCipher = new byte[1024];
+		Scanner sc = new Scanner(System.in);
+	        
+		byte[] ciphertext = new byte[1024]; 
+		byte[] prevCipher = new byte[1024];
 
-	callEncrypt(null, 0, ciphertext);    
-	int lsb = (ciphertext[6] & 0xFF) * 256 + (ciphertext[7] & 0xFF), plsb = 0;
-	
-	// the ciphertext is 64 bytes long, hence the padded plaintext is 56 bytes long
-	// the IV is approximately 5 times the timestamp in milliseconds
-	// when run through ssh the code below gives approximately 5000 difference in the IV
-	
-	byte[] msg = new byte[8];
+		byte[] msg = new byte[8];
 
-	for(int pos = 0; pos < 8; pos++){
-
-	byte[] expIV = nextIV(ciphertext);
-	boolean found = false;
-	
-	int cnt = 0;
-
-	while(!found && cnt < 300){
-		cnt += 1;
+		callEncrypt(null, 0, ciphertext);    
 		
-		byte[] prefix = new byte[7 - pos];
-		Arrays.fill(prefix, (byte) 0);
-		expIV = nextIV(ciphertext);
-		callEncrypt(prefix, 7 - pos, ciphertext);
-
-		found = Arrays.equals(expIV, subArray(ciphertext, 0, 8));
-	}
-	if(!found) return;
-	// this is the encryption of m_{8 - pos} xor iv_8
-	byte tar = ciphertext[8 + 7 - pos];
-	byte iv_8 = ciphertext[7];
-	if(found) System.out.println("Sucess, target is " + tar + " iv_8 is " + iv_8);
-
-	boolean match = false;
-	boolean end = false;
-
-	for(byte candidate = -128; !end; candidate++){
-		if(candidate == 127) end = true;
-
-		plsb = lsb;
-		prevCipher = ciphertext;
+		// the ciphertext is 64 bytes long, hence the padded plaintext is 56 bytes long
+		// the IV is approximately 5 times the timestamp in milliseconds
+		// when run through ssh the code below gives approximately 5000 difference in the IV
 		
-		byte[] prefix = merge(pos, msg, candidate);
-		boolean guess = false;
 
-		while(!guess){
-			expIV = nextIV(ciphertext);
-			callEncrypt(prefix, prefix.length, ciphertext);
-			guess = Arrays.equals(expIV, subArray(ciphertext, 0, 8));
-		}
-		if (ciphertext[15] == tar){
-			System.out.println("Candidate " + candidate + " is a match");
-			msg[pos] = (byte) (candidate ^ iv_8);
-			match = true;
-			break;
-		}
-		else{
-			System.out.println("Candidate " + candidate +" is rejected");
-			continue;
-		}
-//		lsb = (ciphertext[6] & 0xFF) * 256 + (ciphertext[7] & 0xFF);
-//		int diff = (lsb - plsb + 256 * 256) % (256 * 256);
-//		System.out.println("Difference of IV: " + diff + " " +  String.format("%#x", diff));
-	}
-	
-	if(!match){ 
-		System.out.println("\nxXx No match found xXx\n");
-		return;
-	}
-	System.out.println("\nMessage is " + (char) msg[pos]);
+		for(int pos = 0; pos < 8; pos++){
 
-	for(byte b : msg){
-		System.out.print( (char) b);
-	}    
-	System.out.println("");
-    }
+			// for each position find the target ciphertext
+			// need 7 - pos zeroes before the text
+			byte[] prefix = new byte[7 - pos];
+			Arrays.fill(prefix, (byte) 0);
+
+			guessIV(pos, ciphertext, prefix);
+
+			// E(msg[pos] xor iv_8) is at ciphertext 15
+			byte tar = ciphertext[15];
+			byte iv_8 = ciphertext[7];
+
+			System.out.println("Sucess, target is " + tar + " iv_8 is " + iv_8);
+
+
+			boolean match = false;
+			boolean end = false;
+
+			for(byte candidate = -128; !end; candidate++){
+				if(candidate == 127) end = true;
+				
+				byte[] prefix = merge(pos, msg, candidate);
+				System.out.print("Prefix is :" )
+				for(byte b : prefix) System.out.print(" " + b)
+
+				guessIV(ciphertext, prefix);	
+
+				if (ciphertext[15] == tar){
+					System.out.println("Candidate " + candidate + " is a match");
+					msg[pos] = (byte) (candidate ^ iv_8);
+					match = true;
+					break;
+				}
+				else{
+					System.out.println("Candidate " + candidate +" is rejected");
+					continue;
+				}
+			}
+			
+			if(!match){ 
+				System.out.println("\nxXx No match found xXx\n");
+				return;
+			}
+			System.out.println("\nMessage is " + (char) msg[pos]);
+		}
+
+
+		for(byte b : msg){
+			System.out.print( (char) b);
+		}    
+		System.out.println("");
+	    
     }
     
 

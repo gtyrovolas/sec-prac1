aaaf07b25c964b72df01f893cc7ebc73d2b3f7d8
Giannis Tyrovolas <johntyro@hotmail.com>
2020-12-07 01:08:13 +0000

    initial code for task2

--- BeastAttack.java
+++ BeastAttack.java
@@ -7,51 +7,99 @@ import java.util.concurrent.TimeUnit;
 public class BeastAttack
 {
 
+    public static byte[] subArray(byte[] array, int beg, int end) {
+        return Arrays.copyOfRange(array, beg, end + 1);
+    }
+
     static void printCT(byte[] ct){
 	
-	for(int i = 0; i < 8; i++)
-	{
-	    System.out.print(String.format("%02x ", ct[i]));
-	}
-	
-	for(int j = 8; j < ct.length; j++)
-	{
-	    System.out.print(String.format("%02x", ct[j]));
-	}
-	System.out.println("");    
+		for(int i = 0; i < 8; i++)
+		{
+		    System.out.print(String.format("%02x ", ct[i]));
+		}
+		
+		for(int j = 8; j < ct.length; j++)
+		{
+		    System.out.print(String.format("%02x", ct[j]));
+		}
+		
+		System.out.println("");    
+    }
+
+    static byte[] nextIV(byte[] ct){
+    	byte[] expIV = subArray(ct, 0, 8);
+    	// next IV guess, based on cycles of 13 ticks
+		expIV[7] += 13;
+		if(expIV[7] <= -128 + 13)
+			expIV[6] += 1;
+		return expIV;
     }
 
     public static void main(String[] args) throws Exception
     {
+
 	Scanner sc = new Scanner(System.in);
         
 	byte[] ciphertext = new byte[1024]; 
 	byte[] prevCipher = new byte[1024];
 
-	callEncrypt(null, 0, ciphertext);	    
-
+	callEncrypt(null, 0, ciphertext);    
+	int lsb = (ciphertext[6] & 0xFF) * 256 + (ciphertext[7] & 0xFF), plsb = 0;
+	
 	// the ciphertext is 64 bytes long, hence the padded plaintext is 56 bytes long
 	// the IV is approximately 5 times the timestamp in milliseconds
 	// when run through ssh the code below gives approximately 5000 difference in the IV
 	
-	int lsb = (ciphertext[5] & 0xFF) * 256 * 256 + (ciphertext[6] & 0xFF) * 256 + (ciphertext[7] & 0xFF), plsb = 0;
+	byte[] msg = new byte[8];
 
-	for(int k = 1; k < 100; k++){
-	
-	plsb = lsb;
-	prevCipher = ciphertext;
-	
-	byte[] prefix = {0, 0, 0, 0, 0, 0, 0};
-	int length = callEncrypt(prefix, prefix.length, ciphertext);	    
-	
+	int pos = 0;
 
-	lsb = (ciphertext[5] & 0xFF) * 256 * 256 + (ciphertext[6] & 0xFF) * 256 + (ciphertext[7] & 0xFF);
-	int diff = (lsb - plsb + 256 * 256 * 256) % (256 * 256 * 256);
-	System.out.println("Difference of IV: " + diff + " " +  String.format("%#x", diff));
+	byte[] expIV = nextIV(ciphertext);
+	boolean found = false;
 
+	while(found){
+		byte[] prefix = new byte[7];
+		Arrays.fill(prefix, (byte) 0);
+		callEncrypt(prefix, 7, ciphertext);
 
+		if(expIV.equals(subArray(ciphertext, 0, 8))) found = true;
 	}
 
+	// this is the encryption of m_{8 - pos} xor iv_8
+	byte tar = ciphertext[8 + 7 - pos];
+	byte iv_8 = ciphertext[7];
+
+
+	for(byte candidate = -128; candidate < 128; candidate++){
+
+		plsb = lsb;
+		prevCipher = ciphertext;
+		
+		byte[] prefix = {0, 0, 0, 0, 0, 0, 0, candidate};
+		expIV = nextIV(ciphertext);
+		
+
+		int length = callEncrypt(prefix, prefix.length, ciphertext);
+
+		if(expIV.equals(subArray(ciphertext, 0, 8))){
+			if (ciphertext[15] == tar){
+				msg[pos] = (byte) (candidate ^ iv_8);
+				break;
+			}
+			else{
+				continue;
+			}
+		}
+		
+
+		lsb = (ciphertext[6] & 0xFF) * 256 + (ciphertext[7] & 0xFF);
+		int diff = (lsb - plsb + 256 * 256) % (256 * 256);
+		System.out.println("Difference of IV: " + diff + " " +  String.format("%#x", diff));
+	}
+
+	System.out.println("Message is " + (char) msg[0]);
+
+    
     }
     
 
